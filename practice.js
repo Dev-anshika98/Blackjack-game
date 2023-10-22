8065cd7224454910e9b74582a1451fcfab0c27f1
Dev-anshika98 <g.anshikacreative@gmail.com>
2023-10-22 13:44:17 +0530

    practice

---
+++ practice.js
@@ -0,0 +1,50 @@
+let firstcard = 16
+let secondcard = 5
+let sum = firstcard + secondcard
+
+if(sum <= 20){
+    console.log("Do you want to draw a new card? 🙂")
+}
+else if(sum === 21){
+    console.log("Wohoo! You've got Blackjack! 🥳")
+}
+else {
+    console.log("You're out of the game! 😭")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let age = 22
+// if (age <= 20){
+//     console.log("You can not enter the club!")
+// }
+// else{
+//     console.log("Welcome!!")
+// }
+
+// let age = 200
+// if(age < 100){
+//     console.log("Not elegible")
+// }
+// else if(age === 100){
+//     console.log("Here is your birthday card from the King!")
+// }else{
+//     console.log("Not elegible, you have already gotten one")
+// }
